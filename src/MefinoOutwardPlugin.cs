c616e5ab6fc059148d2e49bcf977d07db57ce7f1
Sinai <outlander.sinai@gmail.com>
2021-02-06 01:33:46 +1100

    test init

---
+++ src/MefinoOutwardPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+
+namespace Mefino.Plugin
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class MefinoOutwardPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "io.mefino.outwardplugin";
+        public const string NAME = "Mefino Outward Plugin";
+        public const string VERSION = "0.1.0.0";
+    }
+}
